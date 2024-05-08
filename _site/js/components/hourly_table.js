70964bbf6027d85ff40b25e0aff822026b776f61
Shane Dunne <shanejjdunne@hotmail.com>
2024-05-08 13:53:41 +0100

    add functionality to highlight current hour on the hourly weather table

--- _site/js/components/hourly_table.js
+++ _site/js/components/hourly_table.js
@@ -18,9 +18,25 @@ function createHourlyTable(city, day, preferences) {
     sliceLocation[day].end
   );
 
+  // HIGHLIGHT COLUMN OF CURRENT HOUR
+  // get the current hour
+  const currentHour = new Date().getHours();
+
+  // get the index of the current hour in the timeRowData and use it to find the correct columns
+  const currentHourIndex = timeRowData.findIndex((time) => parseInt(time.slice(-5, -3)) === currentHour);
+
+  /* A function to check if the index of the data being mapped matches the index of the current hour
+   also ensures day === 0, meaning the current hour will only be highlighted on todays hourly table.
+   Sets a class of highlighted-hour to columns replresenting this hour
+   */
+
+  highlightCurrentHourData = (index) => index === currentHourIndex && day === 0 ? "highlighted-hour" : "";
+  
+  
+
   // create row for time data
   let timeRow = `<tr><th class="tg-0lax">Time</th>${timeRowData
-    .map((time) => `<td class="tg-0lax time">${time.slice(-5)}</td>`)
+    .map((time, index) => `<td class="tg-0lax time ${highlightCurrentHourData(index)}">${time.slice(-5)}</td>`)
     .join("")}</tr>`;
 
   // add timeRow to the rows array
@@ -35,7 +51,7 @@ function createHourlyTable(city, day, preferences) {
   );
   // create row for temperature data
   let tempRow = `<tr><th class="tg-0lax">Temp:</th>${tempRowData
-    .map((temp) => `<td class="tg-0lax">${Math.round(temp)}°C</td>`)
+    .map((temp, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${Math.round(temp)}°C</td>`)
     .join("")}</tr>`;
 
   // add tempRow to the rows array
@@ -54,7 +70,7 @@ function createHourlyTable(city, day, preferences) {
 
   // create row for feels like temperature data
   let feelsLikeRow = `<tr><th class="tg-0lax">Feels like:</th>${feelsLikeRowData
-    .map((feelsLike) => `<td class="tg-0lax">${Math.round(feelsLike)}°C</td>`)
+    .map((feelsLike, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${Math.round(feelsLike)}°C</td>`)
     .join("")}</tr>`;
 
   // add feelsLikeRow to the rows array
@@ -73,7 +89,7 @@ function createHourlyTable(city, day, preferences) {
 
   // create row for rain data
   let rainRow = `<tr><th class="tg-0lax">Rain:</th>${rainRowData
-    .map((rain) => `<td class="tg-0lax">${rain}mm</td>`)
+    .map((rain, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${rain}mm</td>`)
     .join("")}</tr>`;
 
   // add rainRow to the rows array
@@ -91,7 +107,7 @@ function createHourlyTable(city, day, preferences) {
 
   // create row for chance of rain data
   let rainChanceRow = `<tr><th class="tg-0lax">Chance of Rain:</th>${rainChanceRowData
-    .map((chance) => `<td class="tg-0lax">${chance}%</td>`)
+    .map((chance, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${chance}%</td>`)
     .join("")}</tr>`;
 
   // add rainChanceRow to the rows array
@@ -109,7 +125,7 @@ function createHourlyTable(city, day, preferences) {
 
   // create row for humidity data
   let humidityRow = `<tr><th class="tg-0lax">Humidity:</th>${humidityRowData
-    .map((humidity) => `<td class="tg-0lax">${humidity}%</td>`)
+    .map((humidity, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${humidity}%</td>`)
     .join("")}</tr>`;
 
   // add humidityRow to the rows array
@@ -127,7 +143,7 @@ function createHourlyTable(city, day, preferences) {
 
   // create row for wind speed data
   let windSpeedRow = `<tr><th class="tg-0lax">Wind Speed:</th>${windSpeedRowData
-    .map((windSpeed) => `<td class="tg-0lax">${Math.round(windSpeed)}km/h</td>`)
+    .map((windSpeed, index) => `<td class="tg-0lax ${highlightCurrentHourData(index)}">${Math.round(windSpeed)}km/h</td>`)
     .join("")}</tr>`;
 
   // add windSpeedRow to the rows array
